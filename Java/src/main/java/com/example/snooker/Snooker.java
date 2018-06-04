1c9f3a60521474c9562db4d4014ec3bb6f709de8
Uri Goldstein <uri.goldstein@gmail.com>
2018-06-04 09:27:31 +0300

    Added comments to Snooker class

--- Java/src/main/java/com/example/snooker/Snooker.java
+++ Java/src/main/java/com/example/snooker/Snooker.java
@@ -6,14 +6,17 @@ public class Snooker {
 		
 	}
 	
+	// returns the new "current player"
 	public String recordMiss(String playerName) {
 		return "";
 	}
 
+	// returns the player's new score
 	public int recordShot(String playerName, Sink shotResult) {
 		return 0;
 	}
 	
+	// returns the score of both players
 	public Score getScore() {
 		return new Score(0, 0);
 	}
